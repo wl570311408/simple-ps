cc10779cd17f663ce558be7d18aeabce392a9ae5
wuliang2026 <wuliang2026@qq.com>
2026-05-09 12:49:33 +0800

    add base config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/simple-ps/', // ← 就加这一行！仓库名就是你的子路径
   plugins: [vue()],
   resolve: {
     alias: {
